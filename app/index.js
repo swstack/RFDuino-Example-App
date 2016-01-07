68697a4a279d338b64f7a0090f82b7af0034b0e4
Stephen Stack <sstack@punchthrough.com>
2016-01-07 16:01:09 -0600

    app: simple app that connects to RFDuino over BLE

--- app/index.js
+++ app/index.js
@@ -1,2 +1,25 @@
-// TODO: Noble app that connects to RFDuino
+var noble = require('noble')
 
+var RFDUINO_NAME = 'RFDuino'
+
+noble.on('stateChange', function(state) {
+  if (state === 'poweredOn') {
+    noble.startScanning()
+  } else {
+    noble.stopScanning()
+  }
+});
+
+noble.on('discover', function(peripheral) {
+  var adv = peripheral.advertisement
+  var name = adv.localName ? adv.localName : 'Unknown Name'
+  if (name == RFDUINO_NAME) {
+  	noble.stopScanning()
+  	peripheral.connect(function(err) {
+      if (err)
+      	console.log('Error: ' + err)
+      else
+      	console.log('Connected.')
+  	})
+  }
+})
