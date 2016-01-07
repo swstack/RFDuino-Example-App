982554ee7ff8ab010254e9f4a43c6beb5f497e0f
Stephen Stack <sstack@punchthrough.com>
2016-01-07 15:20:48 -0600

    init: first commit w/ project structure and files

---
+++ app/index.js
@@ -0,0 +1,2 @@
+// TODO: Noble app that connects to RFDuino
+
---
+++ arduino/ble-basic/ble-basic.ino
@@ -0,0 +1,18 @@
+void setup() {
+  RFduinoBLE.advertisementData = "foobar";
+  RFduinoBLE.deviceName = "RFDuino";
+  RFduinoBLE.begin();
+
+}
+
+void loop() {
+  
+}
+
+void RFduinoBLE_onConnect(){
+// Insert code
+}
+
+void RFduinoBLE_onDisconnect(){
+// Insert code here
+}
